1aed64fa5ffa91fa0f18576782c9325b214fd97b
zadeev <zadeev@gmail.com>
2014-04-27 15:25:26 +0400

    nil

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Higay::Application.config.secret_key_base = '3c973b590a7f26a6838fb9c57787aaf1f74ad8bc73026a417bf912c5a6c10802699a45851b7e8de1603174e26ee37a6f1f7ccc493f02aad8462f7eb404882a44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Higay::Application.config.session_store :cookie_store, key: '_Higay_session'
